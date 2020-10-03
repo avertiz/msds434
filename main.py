bd97e55c3f39fd10689f06bb979eb4f3e470c07b
Andres Vertiz <andre.vertiz@gmail.com>
2020-10-03 20:32:18 +0000

    Hello World Function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def test():
-    return("Hello")
+def hello():
+    hello = {"value": "Hello World"}
+    hello = jsonify(hello)
+    return(hello)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port = 8080, debug = True)
